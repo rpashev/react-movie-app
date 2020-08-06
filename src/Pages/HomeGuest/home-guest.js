834aada7642eb81006af33a8105d38976056c04a
rpashev <56789393+rpashev@users.noreply.github.com>
2020-08-06 16:20:59 +0300

    worked on styling

--- src/Pages/HomeGuest/home-guest.js
+++ src/Pages/HomeGuest/home-guest.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Layout from '../../Components/Layout/Layout'
 import { Link } from 'react-router-dom'
-
+import styles from './home-guest.module.css'
 const HomeGuest = () => {
     return (
         <Layout>
-            <h1>Welcome!</h1>
-            <div>
-                <Link to='/register'><h3>SIGN UP to begin</h3></Link>
-                <p>Already have an account? </p>
-                <Link to='/login'><h3>SIGN IN here</h3></Link>
+            <div className={styles.homeguest}>
+                <h1>Welcome!</h1>
+                <div>
+                    <Link to='/register'><h3>SIGN UP to begin</h3></Link>
+                    <p>Already have an account? </p>
+                    <Link to='/login'><h3>SIGN IN here</h3></Link>
+                </div>
             </div>
+
         </Layout>
 
     )
